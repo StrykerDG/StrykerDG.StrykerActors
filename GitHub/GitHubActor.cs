90a4deb5d46adc51e7efd0410f164da7d79e4c03
Kevin Williams <kevin.williams@vorbild.software>
2020-08-13 19:03:19 -0500

    Initial Commit

---
+++ GitHub/GitHubActor.cs
@@ -0,0 +1,18 @@
+﻿using Akka.Actor;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrykerDG.StrykerActors.GitHub
+{
+    public class GitHubActor : ReceiveActor
+    {
+        private IServiceScopeFactory ServiceScopeFactory { get; set; }
+
+        public GitHubActor(IServiceScopeFactory factory)
+        {
+            ServiceScopeFactory = factory;
+        }
+    }
+}
